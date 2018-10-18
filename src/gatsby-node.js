ae4bd6196226726c7ecd673c4b69e4566bad61f0
Manuel Wieser <office@manuelwieser.com>
2018-10-18 21:27:07 +0200

    Update gatsby-node.js

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,105 +1,100 @@
-const butter = require('buttercms')
-const crypto = require('crypto')
+const butter = require('buttercms');
+const crypto = require('crypto');
 
-const typePrefix = 'butter__'
+const typePrefix = 'butter__';
 
 const refactoredEntityTypes = {
   post: `${typePrefix}POST`,
   contentField: `${typePrefix}CONTENT`
-}
+};
 
-/* Main
-** ***************************** */
 exports.sourceNodes = async (
-  { boundActionCreators, getNode, hasNodeChanged, store },
-  { authToken, contentFields: { keys: contentFieldKeys = [], ...contentFieldOptions } }
+  { actions, getNode, getNodes, createNodeId, hasNodeChanged, store },
+  {
+    authToken,
+    contentFields: { keys: contentFieldKeys = [], ...contentFieldOptions }
+  }
 ) => {
-  const {
-    createNode,
-    touchNode,
-    setPluginStatus
-  } = boundActionCreators
+  const { createNode, touchNode, setPluginStatus } = actions;
 
-  // Authenticate butter api client
-  const api = butter(authToken)
+  // Authenticate ButterCMS API client.
+  const api = butter(authToken);
 
-  // Touch existing ButterCMS nodes so Gatsby doesn't garbage collect them.
+  // Touch existing ButterCMS nodes so Gatsby doesn’t garbage collect them.
   Object.values(store.getState().nodes)
     .filter(n => n.internal.type.slice(0, 8) === typePrefix)
-    .forEach(n => touchNode(n.id))
+    .forEach(n => touchNode(n.id));
 
   // Fetch posts.
-  let postResult
+  let postResult;
   try {
-    postResult = await api.post.list()
+    postResult = await api.post.list({
+      page_size: Number.MAX_SAFE_INTEGER
+    });
   } catch (err) {
-    console.log('error fetching posts', err)
+    console.log('Error fetching posts', err);
   }
 
-  const posts = postResult.data.data
+  const posts = postResult.data.data;
   posts.forEach(post => {
     const gatsbyPost = Object.assign(
-      {},
+      { date: new Date(post.published).toLocaleDateString('en-US') },
       post,
       {
-        id: post.slug,
+        id: createNodeId(post.slug),
+        parent: null,
         children: [],
-        parent: '__SOURCE__',
         internal: {
-          type: refactoredEntityTypes.post
+          type: refactoredEntityTypes.post,
+          mediaType: `application/json`,
+          contentDigest: crypto
+            .createHash(`md5`)
+            .update(JSON.stringify(post))
+            .digest(`hex`)
         }
       }
-    )
+    );
 
-    // get content digest of post
-    const contentDigest = crypto
-      .createHash('md5')
-      .update(JSON.stringify(gatsbyPost))
-      .digest('hex')
-
-    gatsbyPost.internal.contentDigest = contentDigest
-
-    createNode(gatsbyPost)
-  })
+    createNode(gatsbyPost);
+  });
 
   // Fetch content fields.
-  let contentFieldsResult
+  let contentFieldsResult;
   try {
-    contentFieldsResult = await api.content.retrieve(contentFieldKeys, contentFieldOptions)
+    contentFieldsResult = await api.content.retrieve(
+      contentFieldKeys,
+      contentFieldOptions
+    );
   } catch (err) {
-    console.log('error fetching content fields', err)
+    console.log('Error fetching content fields', err);
   }
 
-  const contentFieldEntries = Object.entries(contentFieldsResult.data.data)
-  contentFieldEntries.forEach(([key, value]) => {
-    const gatsbyContentField = Object.assign(
-      {},
-      key,
-      value,
-      {
+  if (contentFieldsResult.data.data) {
+    const contentFields = Object.entries(contentFieldsResult.data.data);
+    contentFields.forEach(([key, value]) => {
+      const gatsbyContentField = Object.assign({}, key, value, {
         id: key,
+        parent: null,
         children: [],
-        parent: '__SOURCE__',
         internal: {
-          type: refactoredEntityTypes.contentField
+          type: refactoredEntityTypes.contentField,
+          contentDigest: crypto
+            .createHash(`md5`)
+            .update(JSON.stringify([key, value]))
+            .digest(`hex`)
         }
-      }
-    )
+      });
 
-    // get content digest of content field
-    const contentDigest = crypto
-      .createHash('md5')
-      .update(JSON.stringify(gatsbyContentField))
-      .digest('hex')
+      createNode(gatsbyContentField);
+    });
+  }
 
-    gatsbyContentField.internal.contentDigest = contentDigest
-
-    createNode(gatsbyContentField)
-  })
+  // Fetch pages.
+  // TODO
 
   setPluginStatus({
     status: {
-      lastFetched: new Date().toJSON()
+      lastFetched: Date.now()
     }
-  })
-}
+  });
+};
