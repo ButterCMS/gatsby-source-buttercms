be3a62ffebaa77e9ca9968520ccc8e88d647a23f
Jon Palustre <jonalecp@gmail.com>
2020-08-29 03:26:28 +0800

    Updated source plugin to support webhook events

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,48 +1,63 @@
-const butter = require('buttercms');
-const crypto = require('crypto');
+const butter = require("buttercms")
+const crypto = require("crypto")
 
-const typePrefix = 'butter__';
+const typePrefix = "butter__"
 
 const refactoredEntityTypes = {
   post: `${typePrefix}POST`,
   page: `${typePrefix}PAGE`,
   contentField: `${typePrefix}CONTENT_FIELD`,
-  collection: `${typePrefix}COLLECTION`
-};
+  collection: `${typePrefix}COLLECTION`,
+}
 
-exports.sourceNodes = async ({
-  actions,
-  getNode,
-  getNodes,
-  createNodeId,
-  hasNodeChanged,
-  store
-}, {
-  authToken,
-  contentFields,
-  pageTypes,
-  locales,
-  preview
-}) => {
-  const {
-    createNode,
-    touchNode,
-    setPluginStatus
-  } = actions;
+exports.sourceNodes = async (
+  { actions, getNode, getNodes, createNodeId, hasNodeChanged, store, webhookBody },
+  { authToken, contentFields, pageTypes, locales, preview }
+) => {
+  const { createNode, deleteNode, touchNode, setPluginStatus } = actions
 
   // Authenticate ButterCMS API client.
-  const api = butter(authToken, false, 20000);
+  const api = butter(authToken, false, 20000)
 
   // Touch existing ButterCMS nodes so Gatsby doesn’t garbage collect them.
   Object.values(store.getState().nodes)
     .filter(n => n.internal.type.slice(0, 8) === typePrefix)
-    .forEach(n => touchNode(n.id));
+    .forEach(n => touchNode(n.id))
 
+  //Support Webhook events to deleteNode and createNode
+  if (webhookBody && webhookBody.type) {
+    const item = webhookBody.data
+    switch (webhookBody.type) {
+      case "post.delete":
+      case "page.delete":
+      case "collectionitem.delete":
+        if (Array.isArray(item) && item.length) {
+          for (const id of item) {
+            deleteNode({
+              node: getNode(id),
+            })
+          }
+        }
+        ƒ
+        break
+      case "post.draft":
+      case "post.published":
+      case "page.draft":
+      case "page.published":
+      case "collectionitem.draft":
+      case "collectionitem.published":
+        createNodeHelper(item, helperObject)
+        break
+      default:
+        break
+    }
+    return
+  }
 
   // Paginate through all blog posts
-  let page = 1;
-  while(page !== null) {
-    let postResult = null;
+  let page = 1
+  while (page !== null) {
+    let postResult = null
     let params = {}
 
     if (preview) {
@@ -51,18 +66,22 @@ exports.sourceNodes = async ({
 
     try {
       postResult = await api.post.list({
-        page: page, page_size: 100, ...params
-      });
+        page: page,
+        page_size: 100,
+        ...params,
+      })
     } catch (err) {
-      console.log('Error fetching posts', err);
+      console.log("Error fetching posts", err)
     }
 
-    let posts = postResult.data.data;
+    let posts = postResult.data.data
     posts.forEach(post => {
-      const gatsbyPost = Object.assign({
-        date: new Date(post.published).toLocaleDateString('en-US')
-      },
-        post, {
+      const gatsbyPost = Object.assign(
+        {
+          date: new Date(post.published).toLocaleDateString("en-US"),
+        },
+        post,
+        {
           id: createNodeId(post.slug),
           parent: null,
           children: [],
@@ -72,24 +91,24 @@ exports.sourceNodes = async ({
             contentDigest: crypto
               .createHash(`md5`)
               .update(JSON.stringify(post))
-              .digest(`hex`)
-          }
+              .digest(`hex`),
+          },
         }
-      );
+      )
 
-      createNode(gatsbyPost);
-    });
+      createNode(gatsbyPost)
+    })
 
     // If there's another page, we paginate
-    page = postResult.data.meta.next_page;
+    page = postResult.data.meta.next_page
   }
-  async function create_nodes_collections (locale) {
+  async function create_nodes_collections(locale) {
     // Fetch content fields.
     if (contentFields) {
       const {
         keys: contentFieldKeys = [],
         ...contentFieldOptions
-      } = contentFields;
+      } = contentFields
 
       var params = {}
 
@@ -98,145 +117,146 @@ exports.sourceNodes = async ({
         params.locale = locale
       }
 
-      var contentFieldsResult;
+      var contentFieldsResult
       try {
         contentFieldsResult = await api.content.retrieve(
           contentFieldKeys,
           contentFieldOptions
-        );
+        )
       } catch (err) {
-        console.log('Error fetching content fields', err);
+        console.log("Error fetching content fields", err)
       }
 
       if (contentFieldsResult.data.data) {
-
         const entries = Object.entries(contentFieldsResult.data.data)
 
         let allCollection = [],
-          allfields = [];
+          allfields = []
 
         //Extract Collections and Content Fields into separate variables
         entries.forEach(([key, value]) => {
-          if (typeof (value) == "object") {
+          if (typeof value == "object") {
             allCollection.push({
               key,
-              value
+              value,
             })
           } else {
             allfields.push({
               key,
-              value
+              value,
             })
           }
         })
 
         //Create Node For Each Collection
-        allCollection.forEach(({
-          key,
-          value
-        }) => {
+        allCollection.forEach(({ key, value }) => {
           if (locale) {
-            var uniqueId = `${locale || 'en'}-${key}`
+            var uniqueId = `${locale || "en"}-${key}`
           } else {
             var uniqueId = key
           }
-          const collectionNode = Object.assign({}, {
-            id: uniqueId,
-            parent: null,
-            children: [],
-            key: key,
-            value: value,
-            internal: {
-              type: refactoredEntityTypes.collection,
-              content: JSON.stringify(value),
-              contentDigest: crypto
-                .createHash(`md5`)
-                .update(JSON.stringify([key, value]))
-                .digest(`hex`)
-            },
-            ...params,
-          })
+          const collectionNode = Object.assign(
+            {},
+            {
+              id: uniqueId,
+              parent: null,
+              children: [],
+              key: key,
+              value: value,
+              internal: {
+                type: refactoredEntityTypes.collection,
+                content: JSON.stringify(value),
+                contentDigest: crypto
+                  .createHash(`md5`)
+                  .update(JSON.stringify([key, value]))
+                  .digest(`hex`),
+              },
+              ...params,
+            }
+          )
 
           createNode(collectionNode)
         })
 
         //Create Node For Each Content Field
-        allfields.forEach(({
-          key,
-          value
-        }) => {
+        allfields.forEach(({ key, value }) => {
           if (locale) {
-            var uniqueId = `${locale || 'en'}-${key}`
+            var uniqueId = `${locale || "en"}-${key}`
           } else {
             var uniqueId = key
           }
-          const contentFieldNode = Object.assign({}, {
-            id: uniqueId,
-            parent: null,
-            children: [],
-            key: key,
-            value: value,
-            internal: {
-              type: refactoredEntityTypes.contentField,
-              content: JSON.stringify(value),
-              contentDigest: crypto
-                .createHash(`md5`)
-                .update(JSON.stringify([key, value]))
-                .digest(`hex`)
-            },
-            ...params,
-          })
+          const contentFieldNode = Object.assign(
+            {},
+            {
+              id: uniqueId,
+              parent: null,
+              children: [],
+              key: key,
+              value: value,
+              internal: {
+                type: refactoredEntityTypes.contentField,
+                content: JSON.stringify(value),
+                contentDigest: crypto
+                  .createHash(`md5`)
+                  .update(JSON.stringify([key, value]))
+                  .digest(`hex`),
+              },
+              ...params,
+            }
+          )
 
           createNode(contentFieldNode)
         })
       }
-
     }
   }
 
-  async function fetch_pages (locale) {
+  async function fetch_pages(locale) {
     // Fetch pages.
     if (!pageTypes) {
       pageTypes = []
     }
-    const pagesResult = [];
+    const pagesResult = []
     var params = {}
 
     if (locale) {
       params.locale = locale
     }
 
-    pageTypes.push('*');
+    pageTypes.push("*")
 
     try {
       for (let i = 0; i < pageTypes.length; i++) {
-        let page = 1;
+        let page = 1
         while (page !== null) {
           let pageTypeResult = null
           pageTypeResult = await api.page.list(pageTypes[i], {
             page: page,
             page_size: 100,
             ...params,
-          });
+          })
           pageTypeResult.data.data.forEach(page => {
             // allButterPage(filter: {page_type: {eq: "page_type"}})
-            page.fields.page_type = pageTypes[i];
-            pagesResult.push(page);
-          });
-          page = pageTypeResult.data.meta.next_page;
+            page.fields.page_type = pageTypes[i]
+            pagesResult.push(page)
+          })
+          page = pageTypeResult.data.meta.next_page
         }
       }
     } catch (err) {
-      console.log('Error fetching page types', err);
+      console.log("Error fetching page types", err)
     }
 
     pagesResult.forEach(page => {
-      const data = Object.assign({
-        slug: page.slug
-      }, page.fields);
+      const data = Object.assign(
+        {
+          slug: page.slug,
+        },
+        page.fields
+      )
 
       if (locale) {
-        var uniqueId = `${locale || 'en'}-${page.slug}`
+        var uniqueId = `${locale || "en"}-${page.slug}`
       } else {
         var uniqueId = page.slug
       }
@@ -251,29 +271,28 @@ exports.sourceNodes = async ({
           contentDigest: crypto
             .createHash(`md5`)
             .update(JSON.stringify(data))
-            .digest(`hex`)
+            .digest(`hex`),
         },
         ...params,
-      });
+      })
 
-      createNode(gatsbyPage);
-    });
+      createNode(gatsbyPage)
+    })
   }
-
 
   if (locales && locales.length > 0) {
     for (const locale of locales) {
-      await create_nodes_collections(locale);
-      await fetch_pages(locale);
+      await create_nodes_collections(locale)
+      await fetch_pages(locale)
     }
   } else {
-    await create_nodes_collections(null);
-    await fetch_pages(null);
+    await create_nodes_collections(null)
+    await fetch_pages(null)
   }
 
   setPluginStatus({
     status: {
-      lastFetched: Date.now()
-    }
-  });
-};
+      lastFetched: Date.now(),
+    },
+  })
+}
